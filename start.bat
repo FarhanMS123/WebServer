e1992c53409827da77b2345a262c72908a2ac9b5
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2020-03-27 20:54:44 +0700

    edit start script and gitignore

--- start.bat
+++ start.bat
@@ -1,1 +1,18 @@
-npm start
+@echo off
+
+echo Checking node_modules...
+IF EXIST node_modules (
+    echo.
+    echo nodu_modules exist
+    echo.
+    echo run npm start
+    npm start
+) ELSE (
+    echo.
+    echo node_modules didn't exist
+    echo run npm install
+    npm install
+    echo.
+    echo run npm start
+    npm start
+)
