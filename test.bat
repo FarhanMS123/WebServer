5a52fd39e16bf7c33cdddc668f7bed86ab2408d6
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2020-03-27 20:55:45 +0700

    edit test script

--- test.bat
+++ test.bat
@@ -1,1 +1,18 @@
-npm test
+@echo off
+
+echo Checking node_modules...
+IF EXIST node_modules (
+    echo.
+    echo nodu_modules exist
+    echo.
+    echo run npm start
+    npm test
+) ELSE (
+    echo.
+    echo node_modules didn't exist
+    echo run npm install
+    npm install
+    echo.
+    echo run npm start
+    npm test
+)
